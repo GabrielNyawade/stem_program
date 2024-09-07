e253ffc560652cdc738106273dd5ce2b42580fd2
Gabriel Opiyo <gabrielopiyo1133@gmail.com>
2024-09-07 19:39:47 +0300

    Optimisatio

--- Python Programming/main_friends.py
+++ Python Programming/main_friends.py
@@ -1,8 +1,17 @@
 from friends import *
 
+def print_friend(friend):
+  print("{} is a {} year old {} and likes {} color" 
+        .format(friend.name, friend.age, friend.gender, friend.fav_color))
+  
 f_1 = Friends("Francis", 23, "Male", "Yellow")
 f_2 = Friends("Patience", 21, "Female", "Green")
 f_3 = Friends("Imelda", 19, "Female", "Lilac")
 
-print("{} is a {} year old {} and likes {} color" .format(f_1.name, f_1.age, f_1.gender, f_1.fav_color))
-print("{} is a {} year old {} and likes {} color" .format(f_3.name, f_3.age, f_3.gender, f_3.fav_color))
+
+f_1.set_name("Francisca")
+f_1.set_gender("Female")
+
+print_friend(f_1)
+print_friend(f_2)
+print_friend(f_3)
